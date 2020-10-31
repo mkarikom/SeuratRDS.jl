35e5926addb3ca4cc6ca3b68c2cfd8c069e2ed31
Matt Karikomi <mattkarikomi@gmail.com>
2020-10-30 23:30:29 -0700

    all tests run

--- src/SeuratRDS.jl
+++ src/SeuratRDS.jl
@@ -1,5 +1,57 @@
+__precompile__()
 module SeuratRDS
 
-# Write your package code here.
+using Pkg
+using Conda
+using Dates
+using RCall
+
+export initR,loadSeur,closeR
+
+# install r to a temp path and return the path
+function initR()
+    currentfn = split(@__FILE__,"/")[end]
+    drnm = string(@__DIR__, "/output/", split(currentfn,".")[1], "/")
+    mkpath(drnm)
+
+    # make a new temporary conda environment for r
+    ts = Dates.time()
+    tdir = "/tmp/r$ts"
+    mkpath(tdir)
+    run(`conda create -p $tdir -y`)
+
+
+    # install r in the temp environment
+    Conda.add_channel("r",tdir)
+    Conda.add("r-base>=3.4.0,<4",tdir) # greater than or equal to 3.4.0 AND strictly less than 4.0
+    return(tdir)
+end
+
+# get the data
+function loadSeur(rdsPath::String,envPath::String,
+              modality::String,assay::String,metadata::String)
+    R"""
+      rdsPath = $rdsPath;
+      seur = readRDS(rdsPath);"""; # read in annotations
+
+    # export counts and embedded labels
+    R"""
+      modality = $modality;
+      assay = $assay;
+      metadata = $metadata;
+      modl = get($modality,slot(seur,"assays"));
+      dat = slot(modl,$assay);
+      met = get(metadata,slot(seur,"meta.data"));
+      print(class(dat));"""
+
+    dat = rcopy(R"as.matrix(dat)")
+    met = rcopy(R"as.matrix(met)")
+    (dat=dat,met=met)
+end
+
+# remove the repository
+function closeR(envPath::String)
+  run(`conda env remove -p $envPath`)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
+using Revise, Test
+using CSV
 using SeuratRDS
-using Test
 
+dn = joinpath(@__DIR__,"data")
+testfn = joinpath(dn,"testSeur.rds")
+checkfn = joinpath(dn,"dataSeur.csv")
 @testset "SeuratRDS.jl" begin
-    # Write your tests here.
+    modality = "RNA"
+    assay = "data"
+    metadata = "nCount_RNA"
+
+    env = initR()
+
+    @test Conda.channels(env) == ["r","defaults"]
+
+    dat = loadSeur(testfn,env,modality,assay,metadata)
+    check = CSV.read(checkfn)
+
+    @test Matrix(check) == dat.dat
+
+    closeR(env)
+    @test !isdir(env)
 end
